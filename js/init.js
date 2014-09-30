1af5df72922a63a3e5c1b2e0e1316b7c3d211b48
“Andreamsofcake” <andrea.moulding@gmail.com>
2014-09-30 14:05:16 -0700

    tooling around with jquery

---
+++ js/init.js
@@ -0,0 +1,10 @@
+
+$( document ).ready(function() {
+  $( "#alert" ).click(function () {
+      alert( "Thanks for visiting!" );
+  });
+  $('#paragraph').click(function () {
+    $('#paragraph').hide();
+  })
+});
+
